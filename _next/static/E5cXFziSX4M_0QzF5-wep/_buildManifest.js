5c4885dc1812a41249673fe1450f465249ca4b2d
ralexander-phi <3751418+ralexander-phi@users.noreply.github.com>
2022-04-10 20:13:26 +0000

    Deploying to gh-pages from @ ralexander-phi/earth-day-word-search@2eca34fde946612cdc29f1ba374ad08a17d651d9 🚀

---
+++ _next/static/E5cXFziSX4M_0QzF5-wep/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/315-b7b113d961db4a77.js","static/css/77122c09a86bf159.css","static/chunks/pages/index-e7a2ad5c2b6807a0.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
